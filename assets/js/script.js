0dda3cbc3d86095f0b48448416e903566d0bc5c6
Rizki Febrian <541231191@student.smktelkom-pwt.sch.id>
2024-08-28 13:16:07 +0700

    login

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+document.getElementById('loginForm'),addEventListener('submit', function(event){
+    event.preventDefault();
+
+    const correctUsername = "Rizki"
+    const correctPassword = "Rizki"
+
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    if (username === correctUsername && password === correctPassword){
+        alert('Login Berhasil');
+        window.location.href = 'index.html';
+    }else{
+        alert('Username atau password salah');
+    }
+})
